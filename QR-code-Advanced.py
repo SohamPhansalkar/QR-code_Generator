37948fbf99c7ad37ed533be61837c4e0baaa30e1
Soham P Phasalkar <sohampp26nov@gmail.com>
2021-10-17 22:51:36 +0530

    Update and rename QR-code.py to QR-code-Advanced.py

--- QR-code-Advanced.py
+++ QR-code-Advanced.py
@@ -3,10 +3,15 @@ from pyqrcode import *
 import png
 
 input_url = input("Put the url which will be then QR-coded : ")
+if input_url == "":
+    input_url = "https://github.com/SohamPhansalkar"
 
 url = pyqrcode.create(input_url)
 
 file_name = input("Write the file name (not to add any extentions EG - .png) : ")
+if file_name == "":
+    file_name = "QR-code"
+
 final_file_name = f"{file_name}.png"
 
 print("Done!")
